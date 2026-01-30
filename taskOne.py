edad2b6b48c52ba5fd1a42a7afa6304559be72fc
MONALISHA BISWAS <bmonalisha1920@gmail.com>
2026-01-30 22:54:22 +0530

    Update taskOne.py

--- taskOne.py
+++ taskOne.py
@@ -1,1 +1,10 @@
+# Task 1 :Build a Calculator CLI App.
+"""Objective: Create a command-line calculator supporting basic operations.
+Tools: Python, VS Code / any text editor, terminal.
+Deliverables: A Python script (calculator.py).
+Hints/Mini Guide:
+1.Use functions for each operation (+, -, *, /)
+2.Take user input using input()
+3.Loop until user chooses to exit"""
 
+print(Hello World)
